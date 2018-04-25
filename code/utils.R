cc9ddb49c1d725e228cc6a90f44e8a841c92a1e4
Mathew Kiang <mathew.kiang@gmail.com>
2018-04-25 08:51:40 -0400

    Better docs for utils

--- code/utils.R
+++ code/utils.R
@@ -1,3 +1,12 @@
+#' Helper function that will clear out the cached images folder
+#' 
+#' Clears out cached images folder (created by Shiny app) by first sorting
+#' through to make sure least used (or oldest) file gets deleted first.
+#'
+#' @param f_path path to cached images folder
+#' @param max_files maximum number of files in there
+#'
+#' @return none
 clean_cached_folder <- function(f_path = normalizePath("./cached_images"), 
                                 max_files = 100) {
   x <- dir_info(f_path)
@@ -15,4 +24,3 @@ clean_cached_folder <- function(f_path = normalizePath("./cached_images"),
     unlink(old_files_to_delete)
   }
 }
-
